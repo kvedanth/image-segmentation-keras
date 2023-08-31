9906af14dc86b7a2dd06420848905a1b9ca64ecf
kvedanth <Kumar.vedantham@gmail.com>
2023-08-31 19:20:26 +0530

    missing comma
    missing comma

--- keras_segmentation/data_utils/data_loader.py
+++ keras_segmentation/data_utils/data_loader.py
@@ -274,7 +274,7 @@ def image_segmentation_generator(images_path, segs_path, batch_size,
                                  augmentation_name="aug_all",
                                  custom_augmentation=None,
                                  other_inputs_paths=None, preprocessing=None,
-                                 mask_encoding=False, mask_colormap=None
+                                 mask_encoding=False, mask_colormap=None,
                                  read_image_type=cv2.IMREAD_COLOR , ignore_segs=False ):
     
 
